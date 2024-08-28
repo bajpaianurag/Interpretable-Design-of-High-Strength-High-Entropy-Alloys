74804d0eab1d96779e12c9e40cc273c9859b0bc8
Anurag Bajpai <93220919+bajpaianurag@users.noreply.github.com>
2024-08-28 13:41:14 +0200

    Update generate_new_compostions_GAN_VAE.py

--- generate_new_compostions_GAN_VAE.py
+++ generate_new_compostions_GAN_VAE.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Lambda
-from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Lambda, LeakyReLU, BatchNormalization, Flatten, Reshape
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras import backend as K
 from sklearn.preprocessing import MinMaxScaler
@@ -17,53 +11,39 @@ from tensorflow.keras.losses import MeanSquaredError
 from tensorflow.keras.optimizers import Adam
 
 
-# In[2]:
-
-
 # Load data
-data = pd.read_csv('final_comp_data.csv')
+data = pd.read_csv('alloy_compositions.csv')
 
 # Preprocess data
 compositions = data.iloc[:, :13].values
 yield_strength = data.iloc[:, 13].values
 
-compositions_normalized = compositions
-
 scaler_yield = MinMaxScaler()
 yield_normalized = scaler_yield.fit_transform(yield_strength.reshape(-1, 1))
-
-
-# In[3]:
-
-
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, LeakyReLU, BatchNormalization, Input, Flatten, Reshape
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.optimizers import Adam
 
 # Generator model
 def build_generator(latent_dim, output_dim):
     model = Sequential()
-    model.add(Dense(128, input_dim=latent_dim))
+    model.add(Dense(64, input_dim=latent_dim))
     model.add(LeakyReLU(alpha=0.2))
     model.add(BatchNormalization(momentum=0.8))
-    model.add(Dense(256))
+    model.add(Dense(86))
     model.add(LeakyReLU(alpha=0.2))
     model.add(BatchNormalization(momentum=0.8))
-    model.add(Dense(512))
+    model.add(Dense(52))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(BatchNormalization(momentum=0.8))
+    model.add(BatchNormalization(momentum=0.4))
     model.add(Dense(output_dim, activation='tanh'))
     return model
 
 # Discriminator model
 def build_discriminator(input_dim):
     model = Sequential()
-    model.add(Dense(512, input_dim=input_dim))
+    model.add(Dense(52, input_dim=input_dim))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(256))
+    model.add(Dense(86))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(128))
+    model.add(Dense(64))
     model.add(LeakyReLU(alpha=0.2))
     model.add(Dense(1, activation='sigmoid'))
     return model
@@ -71,9 +51,9 @@ def build_discriminator(input_dim):
 # Encoder model
 def build_encoder(input_dim, latent_dim):
     model = Sequential()
-    model.add(Dense(512, input_dim=input_dim))
+    model.add(Dense(64, input_dim=input_dim))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(256))
+    model.add(Dense(52))
     model.add(LeakyReLU(alpha=0.2))
     model.add(Dense(latent_dim))
     return model
@@ -89,12 +69,7 @@ def build_gan(generator, discriminator):
     combined.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.28))
     return combined
 
-
-# In[4]:
-
-
-import matplotlib.pyplot as plt
-
+# Plot 2D latent space
 latent_dim = 2
 output_dim = 13
 epochs = 1000
@@ -142,10 +117,6 @@ plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
-
-# In[5]:
-
-
 # Autoencoder for reconstruction
 real_compositions_input = Input(shape=(output_dim,))
 encoded_compositions = encoder(real_compositions_input)
@@ -169,7 +140,6 @@ plt.title('Autoencoder Training Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend()
-plt.savefig('los_epoch_VAE.jpg', dpi=600)
 plt.show()
 
 # Plot the latent space and save the figure
@@ -179,15 +149,10 @@ plt.colorbar(label='Yield Strength')
 plt.title('2D Latent Space')
 plt.xlabel('Latent Dimension 1')
 plt.ylabel('Latent Dimension 2')
-plt.savefig('latent_space_plot.jpg', dpi=600)
 plt.show()
 
-
-# In[6]:
-
-
-# Generate 10,000 new compositions
-num_samples = 1000
+# Generate 5,000 new compositions
+num_samples = 5000
 noise = np.random.normal(0, 1, (num_samples, latent_dim))
 new_compositions = generator.predict(noise)
 
@@ -198,14 +163,8 @@ new_compositions_denormalized = scaler_compositions.inverse_transform(new_compos
 new_compositions_df = pd.DataFrame(new_compositions_denormalized, columns=[f'Element_{i+1}' for i in range(output_dim)])
 new_compositions_df.to_excel('generated_compositions.xlsx', index=False)
 
-print("Generated compositions exported to 'generated_compositions.xlsx'")
-
 # Project the generated compositions into the latent space
 generated_latent_vectors = encoder.predict(new_compositions)
-
-
-# In[ ]:
-
 
 # Plot the latent space and save the figure
 plt.figure(figsize=(10, 7))
@@ -214,18 +173,4 @@ plt.scatter(generated_latent_vectors[:, 0], generated_latent_vectors[:, 1], c='o
 plt.xlabel('Latent Dimension 1')
 plt.ylabel('Latent Dimension 2')
 plt.legend()
-plt.savefig('latent_space_plot_with_generated_CoCrFeMnNiAlCu.jpg', dpi=600)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
