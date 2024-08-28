a0eb7457d1cb6388edd8f640bfa85f0cc0f0e998
Anurag Bajpai <93220919+bajpaianurag@users.noreply.github.com>
2024-08-28 13:34:05 +0200

    Update Data_preprocessing_and_feature_selction.py

--- Data_preprocessing_and_feature_selction.py
+++ Data_preprocessing_and_feature_selction.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,44 +10,32 @@ from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 from sklearn.feature_selection import SelectFromModel
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
-
-# In[ ]:
-
+from scipy.stats import spearmanr
+from sklearn.inspection import permutation_importance
+from sklearn.feature_selection import RFE
+from sklearn.preprocessing import StandardScaler
+import random
 
 # Load the dataset
-data = pd.read_csv("elemental_properties.csv")
-
-
-# In[ ]:
+data = pd.read_csv("your_csv_file.csv")
 
 
 # Check for missing values
 print("Missing values:\n", data.isnull().sum())
 
 
-# In[ ]:
-
-
 # Descriptive statistics
 print("Descriptive statistics:\n", data.describe())
 
-
-# In[ ]:
-
-
+# Histogram of target variable
 sns.set_style('white')
 plt.figure(figsize=(10, 8))
-
-# Histogram of target variable
-sns.histplot(data['YS'], kde=False, color = 'blue', edgecolor='black')
-
+sns.histplot(data['target'], kde=False, color = 'blue', edgecolor='black')
 boxprops = dict(linewidth=2)
-
 plt.xticks(fontname="Times New Roman", fontsize=24)
 plt.yticks(fontname="Times New Roman", fontsize=24)
-plt.xlabel("Yield Strength (MPa)", fontname="Times New Roman", fontsize=27)
-plt.ylabel("Count", fontname="Times New Roman", fontsize=27)
+plt.xlabel("Target", fontname="Times New Roman", fontsize=27)
+plt.ylabel("Counts", fontname="Times New Roman", fontsize=27)
 
 ax = plt.gca()
 for spine in ax.spines.values():
@@ -61,26 +43,15 @@ for spine in ax.spines.values():
 
 ax = plt.gca()
 ax.tick_params(axis="both", which="major", width=3)
-
 sns.despine()
-
 plt.savefig('histogram_YS.png', dpi=1000, bbox_inches='tight')
 
 
-# In[ ]:
-
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-plt.figure(figsize=(12, 8))  # Adjust the figure size
-
+# Create a grid of sub scatter plots
+plt.figure(figsize=(12, 8))
 sns.set_style('white')
-
-data1 = data.drop('YS', axis=1)
+data1 = data.drop('target', axis=1)
 features = data1.columns
-
-# Create a grid of subplots
 num_rows = 4  
 num_cols = 4
 fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 8))
@@ -111,41 +82,20 @@ plt.tight_layout()
 plt.savefig('boxplots_all_features.png', dpi=600)
 plt.show()
 
-
-# In[ ]:
-
-
 # Calculate skewness for each feature
 skewness = data1.skew()
-
-# Display skewness
 print(skewness)
 
-
-# In[ ]:
-
-
+# Apply Yeo-Johnson transformation to descriptors
 cols_to_transform = ['Hmix','Smix','delta','Vm','E','K','G','VEC','Tm','rho','X_P','Omega','eta','E_LD','E_C','F_PN']
-
-# Apply Yeo-Johnson transformation to the selected columns
 pt = PowerTransformer(method='yeo-johnson', standardize=True)
 data[cols_to_transform] = pt.fit_transform(data[cols_to_transform])
 print(data.head())
 
-
-# In[ ]:
-
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-plt.figure(figsize=(12, 8))  # Adjust the figure size
-
+plt.figure(figsize=(12, 8))
 sns.set_style('white')
-
-data1 = data.drop('YS', axis=1)
+data1 = data.drop('target', axis=1)
 features = data1.columns
-
 num_rows = 4  
 num_cols = 4
 fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 8))
@@ -176,33 +126,14 @@ plt.tight_layout()
 plt.savefig('boxplots_transformed_all_features.png', dpi=600)
 plt.show()
 
-
-# In[ ]:
-
-
-# Calculate skewness for each feature
+# Calculate skewness for each transformed feature
 skewness = data1.skew()
-
-# Display skewness
 print(skewness)
 
-
-# In[ ]:
-
-
-data.to_excel('transformed_elemental_properties.xlsx', index=False)
-
-
-# In[ ]:
-
-
+# Create the pairplot with custom styling
 sns.set_style('white')
 plt.figure(figsize=(12, 12))
-
-# Set font size and line width parameters
 sns.set(font_scale=1.2, rc={'lines.linewidth': 1.5})
-
-# Create the pairplot with custom styling
 plot = sns.pairplot(data, diag_kind='hist', diag_kws={'color': 'maroon'}, plot_kws={'color': 'maroon'})
 
 # Set black borders for the plot
@@ -217,134 +148,46 @@ for ax in plot.axes.flatten():
     ax.yaxis.label.set_size(20)
     ax.title.set_size(24)
 
-# Remove spines
-sns.despine()
-
-# Save the plot
-plt.savefig('pairplot_transformed_data.png', dpi=600)
-
-# Show the plot
 plt.show()
 
 
-# In[13]:
+# Feature Selection
 
-
-data1 = pd.read_csv("elemental_properties.csv")
-X1 = data1.drop('YS', axis=1)
-y1 = data1['YS']
-
-font_prop = {'family': 'Arial', 'size': 26}
-
-# Iterate over each feature
-for feature in X1.columns:
-    # Create hexagonal binning plot
-    plt.figure(figsize=(10, 8))
-    plt.hexbin(X1[feature], y1, gridsize=20, cmap='Spectral')
-    plt.xlabel(feature, fontdict=font_prop)
-    plt.ylabel("Yield Strength (MPa)", fontdict=font_prop)
-    plt.colorbar(label="Counts")
-    plt.xticks(fontproperties='Arial', fontsize=24)
-    plt.yticks(fontproperties='Arial', fontsize=24)
-
-    # Save the plot as an image file
-    plt.savefig(f"{feature}_hexbin.png")
-    
-    # Show the plot
-    plt.show()
-
-
-# # Feature Selection
-
-# ### Spearman Rank Correlation
-
-# In[14]:
-
-
-from scipy.stats import spearmanr
+### Spearman Rank Correlation
 
 # Calculate Spearman rank correlation matrix
 corr_matrix_spearman, _ = spearmanr(data)
-
-# Create a heatmap
 sns.heatmap(corr_matrix_spearman, annot=False, cmap='rainbow', vmin=-1, vmax=1)
 plt.title("Spearman Rank Correlation Matrix Heatmap")
 plt.show()
 
 
-# In[15]:
-
-
-# Convert the NumPy array to a pandas DataFrame
-corr_matrix_spearman_df = pd.DataFrame(corr_matrix_spearman, columns=data.columns, index=data.columns)
-output_filename = 'Spearman_correlation_matrix.xlsx'
-
-corr_matrix_spearman_df.to_excel(output_filename, index=False)
-print(f"Spearman correlation matrix saved to {output_filename}")
-
-
-# ### Mutual Importance
-
-# In[16]:
-
-
-X = data.drop('YS', axis=1)
-y = data['YS']
+### Mutual Importance
+X = data.drop('target', axis=1)
+y = data['target']
 
 mi_scores = mutual_info_regression(X, y)
-
-# Print the MI scores for each feature
 for i in range(len(mi_scores)):
     print('Feature %d: %.3f' % (i+1, mi_scores[i]))
 
 
-# ### RF based Feature Selection
+### RF based Feature Selection
 
-# In[17]:
-
-
-# Train a Random Forest regressor
-gbr = RandomForestRegressor(random_state=0)
-gbr.fit(X, y)
-
-# Compute feature importances
-importances = gbr.feature_importances_
+model = RandomForestRegressor(random_state=0)
+model.fit(X, y)
+importances = model.feature_importances_
 
 
-# In[18]:
+### GB based Feature Selection
 
-
-importances
-
-
-# ### GB based Feature Selection
-
-# In[19]:
-
-
-# Train a Gradient Boosting regressor
 gbr = GradientBoostingRegressor(random_state=0)
 gbr.fit(X, y)
-
-# Compute feature importances
 importances = gbr.feature_importances_
 
 
-# In[20]:
-
-
-importances
-
-
-# ### Permutation Importance
-
-# In[21]:
-
-
-from sklearn.inspection import permutation_importance
+### Permutation Importance
 
 model = RandomForestRegressor()
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model.fit(X_train, y_train)
 
@@ -368,15 +211,8 @@ df_importance = df_importance.sort_values(by='Importance', ascending=False)
 print(df_importance)
 
 
-# ### Recursive Feature Elimination
+### Recursive Feature Elimination
 
-# In[22]:
-
-
-from sklearn.feature_selection import RFE
-from sklearn.ensemble import RandomForestRegressor
-
-# Initialize the Random Forest Regressor
 model = RandomForestRegressor()
 
 # Perform Recursive Feature Elimination (RFE)
@@ -396,21 +232,12 @@ df_rankings = df_rankings.sort_values(by='Ranking')
 print(df_rankings)
 
 
-# ### Backward Elimination
+### Backward Elimination
 
-# In[23]:
-
-
-from sklearn.metrics import mean_squared_error
-
-# Initialize the RandomForest Regressor
-model = RandomForestRegressor()
-
-# Initialize the list of selected features
 selected_features = list(X.columns)
 
 # Define a stopping criterion
-stopping_criterion = 0.01  # Adjust this as needed
+stopping_criterion = 0.01
 
 # Initialize lists to store results
 mse_values = []
@@ -464,9 +291,6 @@ while len(selected_features) > 1:
 # Print the final selected features
 print("Selected Features:", selected_features)
 
-# Create a DataFrame to store the results
-be_results_df = pd.DataFrame({'Number of Features': num_features_selected, 'MSE': mse_values})
-
 # Plot the MSE values against the number of selected features
 plt.figure(figsize=(10, 6))
 plt.plot(be_results_df['Number of Features'], be_results_df['MSE'], marker='o', linestyle='-')
@@ -477,23 +301,7 @@ plt.grid(True)
 plt.show()
 
 
-# In[24]:
-
-
-be_results_df
-
-
-# ### Genetic Algorithm Feature Selection
-
-# In[24]:
-
-
-from sklearn.preprocessing import StandardScaler
-import random
-import matplotlib.pyplot as plt
-
-# Split the data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+### Genetic Algorithm Feature Selection
 
 # Standardize the features
 scaler = StandardScaler()
@@ -510,9 +318,9 @@ def fitness_function(selected_features):
     return -mse
 
 # Genetic Algorithm parameters
-population_size = 10
+population_size = 16
 num_generations = 1000
-mutation_rate = 0.3
+mutation_rate = 0.2
 
 # Initialize population randomly
 population = [random.sample(range(X_train.shape[1]), 5) for _ in range(population_size)]
@@ -522,7 +330,6 @@ best_fitness_scores = []
 average_fitness_scores = []
 best_individuals = []
 
-# Main loop of the genetic algorithm
 for generation in range(num_generations):
     # Evaluate fitness of each individual in the population
     fitness_scores = [fitness_function(individual) for individual in population]
@@ -593,20 +400,10 @@ plt.grid(False)
 plt.show()
 
 
-# ### Best-Subset Based Feature Selection
+### Best-Subset Based Feature Selection
 
-# In[ ]:
-
-
-# Define the regression model (you can replace this with any regression model of your choice)
-model = RandomForestRegressor()
-
-# Create a list of all feature indices
 num_features = X.shape[1]
 all_feature_indices = list(range(num_features))
-
-# Create an empty DataFrame to store results
-results_df = pd.DataFrame(columns=["Feature Subset", "Mean Squared Error"])
 
 # Iterate over all possible feature combinations
 for subset_size in range(1, num_features + 1):
@@ -631,13 +428,6 @@ for subset_size in range(1, num_features + 1):
         feature_subset = ",".join([str(i) for i in feature_indices])
         results_df = results_df.append({"Feature Subset": feature_subset, "Mean Squared Error": mse}, ignore_index=True)
 
-# Save the results to an Excel file
-results_df.to_excel("Best_subset_based_feature_selection_results.xlsx", index=False)
-
-
-# In[ ]:
-
-
 # Extract the number of features in each subset
 num_features_in_subset = [len(subset.split(",")) for subset in results_df["Feature Subset"]]
 
@@ -652,10 +442,3 @@ plt.xlabel("Number of Features")
 plt.ylabel("Mean Squared Error")
 plt.grid(True)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
